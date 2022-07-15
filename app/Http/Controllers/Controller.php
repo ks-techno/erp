10835af28662a3e8b85c34aa56d756dcf6ff3732
theumar98 <theumar98@gmail.com>
2022-07-16 01:16:38 +0500

    theme asset setup login design successfully login with ajax base

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,25 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected function jsonSuccessResponse( $data, $message, $statusCode = 200 )
+    {
+        return response()->json(['status'=>'success', 'data'=>(object)$data, 'message'=>$message], $statusCode);
+    }
+
+    protected function jsonErrorResponse( $data, $message,  $statusCode = 203)
+    {
+        return response()->json(['status'=>'error', 'data'=>(object)$data, 'message'=>$message], $statusCode);
+    }
+
+    protected function arrSuccessResponse($data, $message)
+    {
+        return ['status'=>'success', 'data'=>$data, 'message'=>$message];
+    }
+
+    protected function arrErrorResponse($data, $message)
+    {
+        return ['status'=>'error', 'data'=>$data, 'message'=>$message];
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +15,23 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
+    if (Auth::check()) {
+        return redirect('/home');
+    }else {
+        return view('auth.login');
+    }
+});
+
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+
+Auth::routes();
+Route::group(['middleware' => 'auth'], function () {
+
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+
+
 });
