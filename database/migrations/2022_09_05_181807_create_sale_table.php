1f5444b89c914dbd5963f2fce545f5e057d57fd7
Sarah <sarahsajjad93@gmail.com>
2022-09-06 08:18:12 +0500

    sale migration

---
+++ database/migrations/2022_09_05_181807_create_sale_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sale', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid');
+            $table->bigInteger('customer_id');
+            $table->boolean('sale_by_staff');
+            $table->morphs('seller');
+            $table->bigInteger('project_id');
+            $table->bigInteger('product_id');
+            $table->boolean('is_installment')->default(false);
+            $table->boolean('is_booked')->default(false);
+            $table->boolean('is_purchased')->default(false);
+            $table->float('sale_price');
+            $table->float('booked_price')->nullable();
+
+            $table->timestamps();
+
+            $table->foreign('customer_id')->references('id')->on('customer')
+                ->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sale');
+    }
+};
