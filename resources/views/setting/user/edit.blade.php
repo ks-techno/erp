17cf6756a030d06d15d31a9d527c2f3e7ed87837
asnan khalid <asnan.khalid@ks-technologies.net>
2023-03-27 12:11:34 +0500

    commas-amount-bankpayment

--- resources/views/setting/user/edit.blade.php
+++ resources/views/setting/user/edit.blade.php
@@ -26,12 +26,12 @@
                                 @permission($data['permission_edit'])
                                 <a href="{{route('setting.user.edit',$data['id'])}}" class="btn btn-primary btn-sm waves-effect waves-float waves-light">Edit</a>
                                 @endpermission
-                             @endif
+                                @else
+                           <div class="card-link">
+                          <button type="submit" class="btn btn-success btn-sm waves-effect waves-float waves-light">Update</button>
+                          <a href="{{$data['list_url']}}" class="btn btn-secondary btn-sm waves-effect waves-float waves-light">Back</a>
+                        @endif
                         </div>
-                        <div class="card-link">
-                            
-                        <button type="submit" class="btn btn-success btn-sm waves-effect waves-float waves-light">Update</button>
-                            <a href="{{$data['list_url']}}" class="btn btn-secondary btn-sm waves-effect waves-float waves-light">Back</a>
                         </div>
                     </div>
                     <div class="card-body mt-2">
